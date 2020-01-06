8b526cbbffa13c6614136d484f326005ce76f6e0
Tadeusz de Ruijter <46708285+tadeuszderuijter@users.noreply.github.com>
2020-01-06 16:07:48 +0100

    update slug

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -7,7 +7,7 @@ import {getPages, Link, safePrefix} from '../utils';
 
 export default class Blog extends React.Component {
     render() {
-        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/posts'), 'frontmatter.date', 'desc');
+        let display_posts = _.orderBy(getPages(this.props.pageContext.pages, '/'), 'frontmatter.date', 'desc');
         return (
             <Layout {...this.props}>
               <div className="post-feed">
